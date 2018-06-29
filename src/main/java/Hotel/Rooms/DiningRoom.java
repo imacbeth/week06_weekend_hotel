568a74458f35446900e8c40b53545e6217e1f563
Iona Macbeth <ionalmacbeth@gmail.com>
2018-06-29 14:23:06 +0100

    Dining room class set up

--- src/main/java/Hotel/Rooms/DiningRoom.java
+++ src/main/java/Hotel/Rooms/DiningRoom.java
@@ -1,4 +1,14 @@
 package Hotel.Rooms;
 
-public class DiningRoom {
+import Hotel.Guest;
+
+import java.util.ArrayList;
+
+public class DiningRoom extends Room {
+
+    private RoomType roomType;
+
+    public DiningRoom(RoomType roomType, ArrayList<Guest> guests, String roomNumber, double nightlyRate) {
+        super(roomType, guests);
+    }
 }
