092204d85b1ddcbd5acbbf9c9cb8a1a5257b662c
Iona Macbeth <ionalmacbeth@gmail.com>
2018-06-29 16:11:21 +0100

    added wallet to guest

--- src/main/java/Hotel/Guest.java
+++ src/main/java/Hotel/Guest.java
@@ -3,8 +3,9 @@ package Hotel;
 public class Guest {
 
     private String name;
+    private double wallet;
 
-    public Guest(String name){
+    public Guest(String name, double wallet) {
         this.name = name;
     }
 
@@ -15,4 +16,12 @@ public class Guest {
     public void setName(String name) {
         this.name = name;
     }
+
+
+    public double getWallet() {
+        return this.wallet;
+    }
+
+
+
 }
